eb0cf8c85eed9c1655a4a7fefa31258f0ee0213e
Grace Best <136754141+gracesmiles@users.noreply.github.com>
2025-07-20 22:43:22 -0400

    Create player.py

---
+++ src/player.py
@@ -0,0 +1,14 @@
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speed = 5
+        # More attributes like health, score...
+
+    def move(self, direction):
+        # Update player position based on direction
+        pass
+
+    def draw(self, screen):
+        # Draw player sprite on screen
+        pass
