28aa1a39f061f45b59ac061f8637c14fd6a4854b
Grace Best <136754141+gracesmiles@users.noreply.github.com>
2025-07-23 23:24:26 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+from PIL import Image
+
 """
 Game settings (FPS, screen size, colors, etc.)
 """
@@ -9,4 +11,35 @@ SETTINGS = {
     "BG_COLOR": (135, 206, 235),  # Light blue background
     "TITLE": "Simple Platformer",  # Window title
     # New key for background image
+}
+
+PLAYER_SETTINGS = {
+    "speed": 5,         # How fast the player moves left/right
+    "jump_power": 15,   # How high the player jumps
+    "gravity": 0.8,     # Strength of gravity pulling the player down
+}
+
+# Make licorice image horizontal rather than vertical
+img_licorice = Image.open("assets/images/red_licorice.png")
+licorice = img_licorice.rotate(-90, expand=True)
+licorice.save("assets/images/horizontal_red_licorice.png")
+
+PLATFORM_SETTINGS = {
+    "licorice_normal": {
+        "image": "assets/images/horizontal_red_licorice.png",
+        "width": 100,
+        "height": 20
+    },
+    "licorice_moving": {
+        "image": "assets/images/horizontal_red_licorice.png",
+        "width": 120,
+        "height": 20,
+        "speed": 2
+    }   
+}
+
+GAME_STATES = {
+    "MENU": "menu",
+    "PLAYING": "playing",
+    "GAME_OVER": "game_over"
 }
