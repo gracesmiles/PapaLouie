2f6c824c247bb6e4b120c572918d236bced73b9f
Grace Best <136754141+gracesmiles@users.noreply.github.com>
2025-07-23 23:56:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from settings import SETTINGS  # Import game settings
+from settings import SETTINGS
+from classes.player import Player
 
 # Initialize Pygame
 pygame.init()
@@ -8,17 +9,26 @@ pygame.init()
 screen = pygame.display.set_mode((SETTINGS["WIDTH"], SETTINGS["HEIGHT"]))
 pygame.display.set_caption(SETTINGS["TITLE"])
 
-# Game loop
-print("Game Running!")
+# Create player instance
+player = Player(100, 500)
 
+# Game loop
 running = True
 while running:
+    keys = pygame.key.get_pressed()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill(SETTINGS["BG_COLOR"])  # Set background color
-    pygame.display.flip()  # Refresh the screen
+    screen.blit(background, (0, 0))  # Draw background
+    screen.blit(player.image, player.rect)  #Draw Player
+    
+    player.move(keys)      # Move the player based on input
+    player.apply_gravity() # Apply gravity using a method from our player class
 
-# Quit Pygame
+    pygame.display.flip()
+
 pygame.quit()
+
