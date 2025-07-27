403ef75727fb05f082adc5f9a53c4e312c1b512d
Grace Best <136754141+gracesmiles@users.noreply.github.com>
2025-07-27 02:44:17 -0400

    Update player.py

--- classes/player.py
+++ classes/player.py
@@ -8,8 +8,8 @@ right1Load = pygame.image.load("assets/images/playerLeft_walk1.png")
 right2Load = pygame.image.load("assets/images/playerLeft_walk2.png")
 right1 = pygame.transform.flip(right1Load, True, False)
 right2 = pygame.transform.flip(right2Load, True, False)
-right1.image.save("assets/images/playerRight_walk1.png")
-right2.image.save("assets/images/playerRight_walk2.png")
+pygame.image.save(right1, "assets/images/playerRight_walk1.png")
+pygame.image.save(right2, "assets/images/playerRight_walk2.png")
 
 
 class Player(pygame.sprite.Sprite):
