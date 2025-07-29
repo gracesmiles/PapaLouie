e06b8f355bfa4299585e664736e1c3e6b893d9a8
Grace Best <136754141+gracesmiles@users.noreply.github.com>
2025-07-29 01:00:27 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -134,7 +134,7 @@ LEVELS = {
             {"x": 720, "y": 170, "type": "coin"}
         ],
         "enemies": [
-            {"x": 300, "y": 390, "type": "fast_enemy"},   # On licorice platform
+            {"x": 300, "y": 360, "type": "fast_enemy"},   # On licorice platform
             {"x": 850, "y": 500, "type": "fast_enemy"},    # On ground platform
             {"x": 1050, "y": 500, "type": "basic_enemy"},  # On ground platform
             {"x": 1450, "y": 500, "type": "fast_enemy"},   # On ground platform
